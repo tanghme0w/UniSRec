ba755d71a5d1b53e9ff7cfdd4ec0f8e43813f17c
Haomiao Tang <thm23@mails.tsinghua.edu.cn>
2024-04-15 19:16:59 +0800

    test on large dataset

--- test/npy_test.py
+++ test/npy_test.py
@@ -1,4 +1,4 @@
 import numpy as np
 
-ue = np.load('../all_user_embedding.npy')
-print(ue[15])
+ue = np.load('/home/me0w/Desktop/hw_rec/UniSRec/dataset/customized/preprocess/mmap/all_user_embedding.npy')
+print(ue[1237][0])
